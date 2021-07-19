caf97f12c56e733af6c3364be3f3f95684ec354c
EqualMa <equalma@outlook.com>
2021-07-19 20:16:33 +0000

    feat: multiple entry points

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export default function hello(name: string): string {
+import { pascalCase } from "pascal-case";
+
+export function hello(name: string): string {
   return `hello ${name}!`;
 }
+
+export function helloPascalCase(name: string): string {
+  return pascalCase(hello(name));
+}
+
+export { default as bye } from "./bye";
+export { default as hi } from "./hi";
+export { Punctuation } from "./util/format";
